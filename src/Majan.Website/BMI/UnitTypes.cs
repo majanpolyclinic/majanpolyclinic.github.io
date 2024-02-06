366e0975317c21a2cd1d87e73d7ff39bb67ec07a
Milad Raeisi <miladsoft@yahoo.com>
2024-02-06 12:46:32 +0400

    Add BMICalculator

---
+++ src/Majan.Website/BMI/UnitTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+      
+ 
+    /// <summary>
+    /// enum UnitTypes
+    /// Description: This list of enums hold values for the 
+    /// designation of measurement unit standards
+    /// </summary>
+    enum UnitTypes
+    {
+        Metric,
+        American
+    }
+ 
